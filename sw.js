19edb70922ef98bd0ad6f3f03ffd6365d48ceba1
THARPU SECONDARY SCHOOL <perturbedhuman@gmail.com>
2026-05-23 00:07:10 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,38 @@
-const CACHE = "tharpu-school-v1";
+const CACHE = "tharpu-school-v2";
+const ASSETS = [
+  "/tharpumavi.github.io/",
+  "/tharpumavi.github.io/index.htm",
+  "/tharpumavi.github.io/manifest.json",
+  "/tharpumavi.github.io/icon.png"
+];
+
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(["/", "/index.html", "/manifest.json", "/icon.png"]))
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(response => {
+        return caches.open(CACHE).then(c => {
+          c.put(e.request, response.clone());
+          return response;
+        });
+      });
+    }).catch(() => caches.match("/tharpumavi.github.io/index.htm"))
   );
 });
